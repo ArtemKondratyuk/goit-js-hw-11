1372b655506709f77b59eb7fae5df6dd3807c309
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2023-06-25 00:49:03 +0300

    update js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,26 @@
-export const API_KEY = '37248711-c91549f463c72d1d85eaef75b';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+let perPage;
+function getApi(inputValue, page) {
+  perPage = 40;
+  const urlAPI = 'https://pixabay.com/api/?';
+  const searchParams = new URLSearchParams({
+    key: '37724353-7d1c51f4a49831c7577e79af3',
+    q: inputValue,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    maxHeight: 300,
+    safesearch: true,
+    per_page: perPage,
+    page: page,
+  });
+  return urlAPI + searchParams.toString();
+}
 
-export function fetchIMG(query, currentPage) {
-  return `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`;
+export async function getRequest(inputValue, page) {
+  const url = getApi(inputValue, page);
+  return axios
+    .get(url)
+    .then(response => response)
+    .catch(error => Notiflix.Notify.warning(error));
 }
